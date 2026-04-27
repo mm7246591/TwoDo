84e8ffe3d4857d3c80da5802ba795e69f1459318
Yan <mm7246591@yahoo.com.tw>
2026-04-27 18:05:55 +0800

    chore: support vue specs and theme tokens

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'vue-spec-plugin',
+      enforce: 'pre', // 在其他外掛程式之前執行
+      transform(_code, id) {
+        // 只處理 .vue 檔案中的 spec 區塊
+        if (/\.vue\?vue&type=spec(?:&|$)/.test(id)) {
+          return { code: 'export default {};', map: null };
+        }
+        return null;
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
